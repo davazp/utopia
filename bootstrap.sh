21cff3afdb0f704ae9e0fd2e6dd97a1dd778212c
David Vázquez Púa <davazp@gmail.com>
2021-03-07 22:59:54 +0100

    Fix bootstrap.sh symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,8 @@ EMACS_DIR=~/.emacs.d
 
 mkdir -p $EMACS_DIR
 
-ln -siv $PWD/emacs/init.el $EMACS_DIR/init.el
-ln -siv $PWD/emacs/lisp/ $EMACS_DIR/lisp
+ln -siv $PWD/emacs/init.el $EMACS_DIR
+ln -siv $PWD/emacs/lisp/ $EMACS_DIR
 
 mkdir -p $EMACS_DIR/straight/
-ln -siv $PWD/emacs/straight/versions $EMACS_DIR/straight/versions
+ln -siv $PWD/emacs/straight/versions $EMACS_DIR/straight
